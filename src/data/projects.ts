3c30e4a55c244b0e071090b3b0fbf5d9f752b01c
Jasiel Rasec <jasielrasec18@gmail.com>
2026-07-30 18:03:33 -0300

    fix: correção de caminho de favicon e logo nova

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,16 +12,6 @@ export const projects: Project[] = [
     githubUrl: 'https://github.com/jasielrasec18-dev/anny-nutricionista',
   },
   {
-    id: 'super-mario-galaxy',
-    title: 'Super Mario Galaxy',
-    description:
-      'Landing page "Super Mario Galaxy" em versão responsiva, com foco em layout e interações visuais para uma experiência imersiva e divertida.',
-    image: '/images/super-mario-galaxy.gif',
-    technologies: ['HTML5', 'CSS3', 'JavaScript', 'GSAP'],
-    liveUrl: 'https://super-mario-galaxy.vercel.app/',
-    githubUrl: 'https://github.com/jasielrasec18-dev/super-mario-galaxy',
-  },
-  {
     id: 'aura-studio',
     title: 'Aura Studio',
     description:
@@ -31,4 +21,14 @@ export const projects: Project[] = [
     liveUrl: 'https://jasielrasec18-dev.github.io/aura-studio/',
     githubUrl: 'https://github.com/jasielrasec18-dev/aura-studio/',
   },
+  {
+    id: 'super-mario-galaxy',
+    title: 'Super Mario Galaxy',
+    description:
+      'Landing page "Super Mario Galaxy" desenvolvida com HTML, CSS, JavaScript e GSAP em versão responsiva, com foco em layout e interações visuais e modernas para uma experiência imersiva e divertida.',
+    image: '/images/super-mario-galaxy.gif',
+    technologies: ['HTML5', 'CSS3', 'JavaScript', 'GSAP'],
+    liveUrl: 'https://super-mario-galaxy.vercel.app/',
+    githubUrl: 'https://github.com/jasielrasec18-dev/super-mario-galaxy',
+  }
 ];
